a7276dad50e90aef28223975a19402dc62496bfe
Samarth292005 <samarthagarwal6277@gmail.com>
2026-05-31 22:07:03 +0530

    Remove EUR and JPY conversion logic
    Removed currency conversion cases for EUR and JPY.

--- CurrencyConverterApp/currencyyconverter (4)/MainActivity.java
+++ CurrencyConverterApp/currencyyconverter (4)/MainActivity.java
@@ -4,16 +4,7 @@
     }
 
     
-            case "EUR": inr = amount * 90; break;
-            case "JPY": inr = amount * 0.55; break;
-        }
-
-        // Convert INR to target
-        switch (to) {
-            case "INR": return inr;
-            case "USD": return inr / 83;
-            case "EUR": return inr / 90;
-            case "JPY": return inr / 0.55;
+            ;
         }
 
         return 0;
